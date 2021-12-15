7e0d1bf09a0fbf910f1e84bb840f3b3e95063790
danilaTernovoi <86415484+danilaTernovoi@users.noreply.github.com>
2021-12-15 21:29:33 +0300

    Update Sandbox.tsx

--- src/Sandbox/Sandbox.tsx
+++ src/Sandbox/Sandbox.tsx
@@ -1,43 +1,7 @@
-import React, { useEffect } from "react";
-import { MultipleArticlesResponseModel, UserResponseModel } from "../types";
-
+import React from "react";
 import "./Sandbox.scss";
 
 const Sandbox = () => {
-  useEffect(() => {
-    (async () => {
-      // LOGIN
-      const loginRes = await fetch("http://kata.academy:8022/users/login", {
-        method: "post",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify({
-          user: {
-            email: "diiiin1@yandex.ru",
-            password: "omgDanilMobil1",
-          },
-        }),
-      });
-
-      const {
-        user: { token, username },
-      }: UserResponseModel = await loginRes.json();
-
-      const updatedUserRes = await fetch(`http://kata.academy/user`, {
-        method: "PUT",
-        headers: {
-          Authorization: `Token ${token}`,
-          "Content-Type": "application/json",
-        },
-
-        body: JSON.stringify({
-          user: {
-            username: "diiiin1",
-          },
-        }),
-      });
-    })();
-  }, []);
-
   return <div>Sandbox</div>;
 };
 
